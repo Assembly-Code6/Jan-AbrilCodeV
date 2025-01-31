11b5a6e67d0f101f995525010c1db00342c2ea8d
Rafael4DC <rafaeldcosta1337@assembly.pt>
2025-01-31 17:58:46 +0000

    added the first API route, but for weather forecast

--- code/Backend/BETArandAPI/Controllers/WeatherForecastController.cs
+++ code/Backend/BETArandAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using BETArandDomain;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BETArandAPI.Controllers
 {
@@ -6,6 +8,10 @@ namespace BETArandAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
+        private List<WeatherForecast> forecasts = new List<WeatherForecast>();
+
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -16,18 +22,56 @@ namespace BETArandAPI.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
-        }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var a = Enumerable.Range(1, 3).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+            forecasts.AddRange(a);
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return forecasts;
+        }
+
+        [HttpDelete("{id}")]
+        public WeatherForecast Delete(int id)
+        {
+            WeatherForecast wf = forecasts[id];
+            forecasts.RemoveAt(id);
+            Console.WriteLine(forecasts.Count);
+            return wf;
+        }
+
+        [HttpGet("{id}")]
+        public WeatherForecast Get(int id)
+        {
+            Console.WriteLine(forecasts.Count);
+            return forecasts[id];
+        }
+
+
+        [HttpPost]
+        public WeatherForecast PostWeatherResults(WeatherForecast wfOg)
+        {
+            WeatherForecast wf = new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                TemperatureC = wfOg.TemperatureC,
+                Summary = wfOg.Summary
+            };
+
+            forecasts.Add(wf);
+
+            Console.WriteLine(forecasts.Count);
+
+            return wf;
         }
     }
 }
